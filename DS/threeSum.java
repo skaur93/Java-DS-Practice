604c03b7dad7d6a41e553570d08eda5acb79ff63
skaur93 <svirk309@gmail.com>
2023-04-21 16:04:51 -0400

    Create threeSum.java

---
+++ DS/threeSum.java
@@ -0,0 +1,53 @@
+package DS;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+//Given an integer array nums,
+//return all the triplets [nums[i], nums[j], nums[k]] 
+//such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+//Notice that the solution set must not contain duplicate triplets.
+
+public class threeSum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int nums[]  = {-1,0,1,2,-1,-4};
+
+		System.out.println(threesum(nums));
+	}
+
+	public static List<List<Integer>> threesum(int[] nums) {
+
+		List<List<Integer>> list = new ArrayList<>();
+
+		//sort
+		Arrays.sort(nums);
+
+		for(int i =0; i<nums.length-2;i++) {
+			if(i==0 || (i>0 && nums[i] != nums[i-1])) {
+				int low = i+1;
+				int high = nums.length-1;
+				while(low<high) {
+					int sum = nums[low] + nums[high];
+					if(nums[i] + sum == 0) {
+						list.add(Arrays.asList(nums[i], nums[low], nums[high]));
+						low++;
+						high--;
+					} else if(sum> -nums[i]) {
+						high--;
+					} else if(sum<-nums[i]) {
+						low++;
+					}
+				}
+			}
+
+			}
+			
+		return new ArrayList<>(list);
+
+	}
+}
